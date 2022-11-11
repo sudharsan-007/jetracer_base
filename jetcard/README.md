74d8d06a7d1be09a10f49b47440e59bf45141589
sudharsan-007 <55453134+sudharsan-007@users.noreply.github.com>
2022-11-11 09:32:28 +0800

    Added Jetcard
    This package runs the display properly, we need this always.

---
+++ jetcard/README.md
@@ -0,0 +1,127 @@
+# JetCard
+
+JetCard is a system configuration that makes it easy to get started with AI.  It comes pre-loaded with
+
+* A Jupyter Lab server that starts on boot for easy web programming
+
+* A script to display the Jetson's IP address (and other stats)
+
+* The popular deep learning frameworks PyTorch and TensorFlow
+
+After configuring your system using JetCard, you can get started prototyping AI projects from your web browser in Python.
+
+If you find an issue, please [let us know](../..//issues)!
+
+## Setup
+
+Follow the steps below to download JetCard directly or create it from scratch.
+
+### Option 1 - Download JetCard directly
+
+1. Download the JetCard image [jetcard_v0p0p0.img](https://drive.google.com/open?id=1wXD1CwtxiH5Mz4uSmIZ76fd78zDQltW_) onto a Windows, Linux or Mac *desktop machine*
+    
+    > You can check it against this [md5sum](https://drive.google.com/open?id=1356ZBrYUWaTgbV50UMB1uCfWrNcd6PEF)
+
+2. Insert a 32GB+ SD card into the desktop machine
+3. Using [Etcher](https://www.balena.io/etcher/) select ``jetcard_v0p0p0.img`` and flash it onto the SD card
+4. Remove the SD card from the desktop machine
+
+You may now insert the SD card into the Jetson Nano, power on, and enjoy the pre-configured system!
+
+> Please note, the password for the pre-built SD card is ``jetson``
+
+### Option 2 - Create JetCard from scratch
+
+1. Flash Jetson Nano following the [Getting Started Guide](https://developer.nvidia.com/embedded/learn/get-started-jetson-nano-devkit)
+
+    > For Jetson TX2 / Xavier, use the [JetPack](https://developer.nvidia.com/embedded/jetpack) SDK manager
+
+2. On the Jetson, run the JetCard installation script
+
+    ```bash
+    git clone https://github.com/NVIDIA-AI-IOT/jetcard
+    cd jetcard
+    ./install.sh <password>
+    ```
+    
+Once the ``install.sh`` script finishes, your system should be configured identically to the SD card image mentioned above.
+
+## Usage
+
+### Connecting
+
+Pick an option below and follow the instructions to begin web programming Jetson from a desktop computer using Jupyter Lab.
+
+#### Option 1 - Ethernet / WiFi
+
+1. Power on the Jetson platform configured using JetCard
+
+2. Connect the Jetson to the same network as your desktop computer via Ethernet or WiFi
+
+    > If you want to connect your Jetson to WiFi, but don't have a monitor and keyboard, you can connect via device mode (see below),       open a terminal, and then use the ``nmcli`` tool to connect to a WiFi network.  Find more details [below](#extras).
+    
+3. Determine the IP address ``jetson_ip_address``
+
+    > If you have the PiOLED display attached, it will display on that screen.  Otherwise, you will need to connect a monitor, open a terminal, and read the IP using ``ifconfig``.
+4. Connect to the Jetson platform from a desktop computer by navigating to ``http://<jetson_ip_address>:8888``
+5. Sign in using the default password ``jetson``
+
+#### Option 2 - USB device mode
+
+If you do not occupy the Jetson Nano's micro USB port for power, you can use it to connect directly from a desktop PC!  The USB device mode IP address is ``192.168.55.1``
+
+1. Power on the Jetson platform configured using JetCard
+
+2. Connect the Jetson platform to the desktop computer via micro USB
+3. On the desktop computer, navigate to ``http://192.168.55.1:8888`` from a web browser
+4. Sign in using the default password ``jetson``
+
+## Extras
+
+### Connect to WiFi from terminal
+
+To connect your Jetson to a WiFi network from a terminal, follow these steps
+
+1. Re-scan available WiFi networks
+
+    ```bash
+    nmcli device wifi rescan
+    ```
+
+2. List available WiFi networks, and find the ``ssid_name`` of your network.
+
+    ```bash
+    nmcli device wifi list
+    ```
+3. Connect to a selected WiFi network
+
+    ```bash
+    nmcli device wifi connect <ssid_name> password <password>
+    ```
+
+### Create SD card snapshot
+
+If you've applied modifications to the base SD card image that you want to re-use, do the following to create a compressed SD card image
+
+1.  Remove the SD card from your Jetson Nano
+
+2.  Insert the SD card into a Linux host computer
+3.  Determine where the SD card is located using ``sudo fdisk -l``.  We'll assume this is at ``/dev/sdb``
+4.  Copy the contents of the SD card to a file named ``jetcard_image.img``
+
+    ```bash
+    sudo dd bs=4M if=/dev/sdb of=jetcard_image.img status=progress
+    ```
+5.  Compress the SD card image using zip
+
+    ```bash
+    zip jetcard_image.zip jetcard_image.img
+    ```
+
+## See also
+
+- [JetBot](http://github.com/NVIDIA-AI-IOT/jetbot) - An educational AI robot based on NVIDIA Jetson Nano
+
+- [JetRacer](http://github.com/NVIDIA-AI-IOT/jetracer) - An educational AI racecar using NVIDIA Jetson Nano
+- [JetCam](http://github.com/NVIDIA-AI-IOT/jetcam) - An easy to use Python camera interface for NVIDIA Jetson
+- [torch2trt](http://github.com/NVIDIA-AI-IOT/torch2trt) - An easy to use PyTorch to TensorRT converter
---
+++ jetcard/install.sh
@@ -0,0 +1,166 @@
+#!/bin/sh
+
+set -e
+
+password='jetson'
+
+# Record the time this script starts
+date
+
+# Get the full dir name of this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Keep updating the existing sudo time stamp
+sudo -v
+while true; do sudo -n true; sleep 120; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Enable i2c permissions
+echo "\e[100m Enable i2c permissions \e[0m"
+sudo usermod -aG i2c $USER
+
+# Install pip and some python dependencies
+echo "\e[104m Install pip and some python dependencies \e[0m"
+sudo apt-get update
+sudo apt install -y python3-pip python3-pil python3-smbus python3-matplotlib cmake
+sudo -H pip3 install --upgrade pip
+sudo -H pip3 install flask
+sudo -H pip3 install --upgrade numpy
+
+# Install jtop
+echo "\e[100m Install jtop \e[0m"
+sudo -H pip install jetson-stats 
+
+
+# Install the pre-built TensorFlow pip wheel
+echo "\e[48;5;202m Install the pre-built TensorFlow pip wheel \e[0m"
+sudo apt-get update
+sudo apt-get install -y libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev
+sudo apt-get install -y python3-pip
+sudo -H pip3 install -U pip setuptools
+sudo -H pip3 install -U numpy grpcio absl-py py-cpuinfo psutil portpicker six mock requests gast h5py==2.10.0 astor termcolor protobuf keras-applications keras-preprocessing wrapt google-pasta
+sudo -H pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v45 tensorflow
+
+# Install the pre-built PyTorch pip wheel 
+echo "\e[45m Install the pre-built PyTorch pip wheel  \e[0m"
+cd
+#wget -N https://nvidia.box.com/shared/static/9eptse6jyly1ggt9axbja2yrmj6pbarc.whl -O torch-1.6.0-cp36-cp36m-linux_aarch64.whl 
+#sudo apt-get install -y python3-pip libopenblas-base libopenmpi-dev 
+#sudo -H pip3 install Cython
+#sudo -H pip3 install numpy torch-1.6.0-cp36-cp36m-linux_aarch64.whl
+
+# Install torchvision package
+echo "\e[45m Install torchvision package \e[0m"
+cd
+#git clone https://github.com/pytorch/vision torchvision
+#cd torchvision
+#git checkout tags/v0.7.0
+#sudo -H python3 setup.py install
+#cd  ../
+pip install 'pillow<7'
+
+# setup Jetson.GPIO
+#echo "\e[100m Install torchvision package \e[0m"
+#sudo groupadd -f -r gpio
+#sudo -S usermod -a -G gpio $USER
+#sudo cp /opt/nvidia/jetson-gpio/etc/99-gpio.rules /etc/udev/rules.d/
+#sudo udevadm control --reload-rules
+#sudo udevadm trigger
+
+# Install traitlets (master, to support the unlink() method)
+echo "\e[48;5;172m Install traitlets \e[0m"
+#sudo -H python3 -m pip install git+https://github.com/ipython/traitlets@master
+sudo python3 -m pip install git+https://github.com/ipython/traitlets@dead2b8cdde5913572254cf6dc70b5a6065b86f8
+
+# Install Jupyter Lab
+echo "\e[48;5;172m Install Jupyter Lab \e[0m"
+sudo apt install -y curl
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt install -y nodejs libffi-dev
+sudo -H pip3 install jupyter jupyterlab
+sudo -H jupyter labextension install @jupyter-widgets/jupyterlab-manager
+
+jupyter lab --generate-config
+python3 -c "from notebook.auth.security import set_password; set_password('$password', '$HOME/.jupyter/jupyter_notebook_config.json')"
+
+# fix for Traitlet permission error
+sudo chown -R jetson:jetson ~/.local/share/
+
+# Install jetcard
+echo "\e[44m Install jetcard \e[0m"
+cd $DIR
+pwd
+sudo -H python3 setup.py install
+
+# Install jetcard display service
+echo "\e[44m Install jetcard display service \e[0m"
+python3 -m jetcard.create_display_service
+sudo mv jetcard_display.service /etc/systemd/system/jetcard_display.service
+sudo systemctl enable jetcard_display
+sudo systemctl start jetcard_display
+
+# Install jetcard jupyter service
+echo "\e[44m Install jetcard jupyter service \e[0m"
+python3 -m jetcard.create_jupyter_service
+sudo mv jetcard_jupyter.service /etc/systemd/system/jetcard_jupyter.service
+sudo systemctl enable jetcard_jupyter
+sudo systemctl start jetcard_jupyter
+
+# Make swapfile
+echo "\e[46m Make swapfile \e[0m"
+cd
+if [ ! -f /var/swapfile ]; then
+	sudo fallocate -l 4G /var/swapfile
+	sudo chmod 600 /var/swapfile
+	sudo mkswap /var/swapfile
+	sudo swapon /var/swapfile
+	sudo bash -c 'echo "/var/swapfile swap swap defaults 0 0" >> /etc/fstab'
+else
+	echo "Swapfile already exists"
+fi
+
+# Install TensorFlow models repository
+echo "\e[48;5;202m Install TensorFlow models repository \e[0m"
+cd
+url="https://github.com/tensorflow/models"
+tf_models_dir="TF-models"
+if [ ! -d "$tf_models_dir" ] ; then
+	git clone $url $tf_models_dir
+	cd "$tf_models_dir"/research
+	git checkout 5f4d34fc
+	wget -O protobuf.zip https://github.com/protocolbuffers/protobuf/releases/download/v3.7.1/protoc-3.7.1-linux-aarch_64.zip
+	# wget -O protobuf.zip https://github.com/protocolbuffers/protobuf/releases/download/v3.7.1/protoc-3.7.1-linux-x86_64.zip
+	unzip protobuf.zip
+	./bin/protoc object_detection/protos/*.proto --python_out=.
+	sudo -H python3 setup.py install
+	cd slim
+	sudo -H python3 setup.py install
+fi
+
+# Disable syslog to prevent large log files from collecting
+#sudo service rsyslog stop
+#sudo systemctl disable rsyslog
+
+# Install jupyter_clickable_image_widget
+echo "\e[42m Install jupyter_clickable_image_widget \e[0m"
+cd
+sudo apt-get install libssl1.0-dev
+#sudo apt-get install nodejs-dev node-gyp libssl1.0-dev
+#sudo apt-get install npm
+git clone https://github.com/jaybdub/jupyter_clickable_image_widget
+cd jupyter_clickable_image_widget
+git checkout no_typescript
+sudo -H pip3 install -e .
+sudo jupyter labextension install js
+sudo jupyter lab build
+
+
+# Install remaining dependencies for projects
+echo "\e[104m Install remaining dependencies for projects \e[0m"
+sudo apt-get install python-setuptools
+
+
+echo "\e[42m All done! \e[0m"
+
+#record the time this script ends
+date
+
